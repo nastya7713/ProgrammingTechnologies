7c36c7d633667db94bca748b51d9623a280f08e7
Anastasia Kostyakina <nastya7713@gmail.com>
2017-11-28 10:05:57 +0200

    init

---
+++ Lab-4/main.rb
@@ -0,0 +1,36 @@
+require 'nokogiri'
+require_relative 'CardsDOMParser'
+require_relative 'CardsSAXParser'
+
+include Nokogiri
+
+xsd = Nokogiri::XML::Schema(File.read('xml/oldCards.xsd'))
+doc = Nokogiri::XML(File.read('xml/oldCards.xml'))
+
+#validation xml agains xsd
+error_list = xsd.validate(doc)
+if error_list.length > 0
+  error_list.each do |error|
+  puts("During validation error occured:")
+  puts error.message
+  end
+else
+  puts('XML is valid')
+end
+
+#parses using DOMParser and shows result
+puts("Parsed by DOM:")
+dom_parser = CardsDOMParser.new(doc)
+old_cards = dom_parser.parse
+puts(old_cards)
+
+#parses using SAXParser and shows result
+sax_parser = CardsSAXParser.new
+parser = XML::SAX::Parser.new(sax_parser)
+parser.parse_file('xml/OldCards.xml')
+puts(old_cards)
+
+#sorts card list
+puts("Sorted:")
+old_cards.sort_by(&:year)
+puts(old_cards.reverse)
